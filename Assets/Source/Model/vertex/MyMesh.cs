25a9815f3d83d642e314fbb4e83fce0c29e98e5f
DrakosXiao <drakosxiao1204@gmail.com>
2018-11-14 16:20:16 -0800

    vertex

---
+++ Assets/Source/Model/vertex/MyMesh.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+partial class MyMesh : MonoBehaviour {
+    public int xSize = 4;
+    public int ySize = 2;
+	// Use this for initialization
+	void Start () {
+        Mesh theMesh = GetComponent<MeshFilter>().mesh;  
+        theMesh.Clear();
+        Vector3[] v = new Vector3[(xSize + 1) * (ySize + 1)];//v is the vertices 
+        Vector3[] n = new Vector3[(xSize + 1) * (ySize + 1)];//n is the normal of each vetices
+        int[] t = new int[xSize*ySize*2 * 3];//xSize*ySize is the number of triangles
+        //initialize the vertices
+        for (int i = 0, y = 0; y <=ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++,i++)
+            {
+                //v[i] = new Vector3(x, y,0);
+                v[i] = new Vector3(x, 0, y);
+            }
+        }
+        //initialize the triangles based on vertices
+        SetTriangles(xSize, ySize,t);
+        //initialize the origial normals
+        for (int i = 0, y = 0; y < ySize; y++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+               n[i] = new Vector3(0, 0, 1);
+            }
+        }
+        //transfer values into mesh
+        theMesh.vertices = v; 
+        theMesh.triangles = t; 
+        theMesh.normals = n;
+        //initialize the sphere on each vertex
+        InitControllers(v);
+        //InitNormals(v, n);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Mesh theMesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] v = theMesh.vertices;
+        Vector3[] n = theMesh.normals;
+        int[] t = theMesh.triangles;
+        for (int i = 0; i < mControllers.Length; i++)
+        {
+            v[i] = mControllers[i].transform.localPosition;
+        }
+
+        ComputeNormals(v,t, n);
+        theMesh.vertices = v;
+        theMesh.normals = n;
+    }
+
+
+
+    void SetTriangles(int xSize, int ySize, int[] triangles)
+    {
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+    }
+}
---
+++ Assets/Source/Model/vertex/MyMesh_Support.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+partial class MyMesh: MonoBehaviour {
+    Vector3 FaceNormal(Vector3[] v, int i0, int i1, int i2)
+    {
+        Vector3 a = v[i1] - v[i0];
+        Vector3 b = v[i2] - v[i0];
+        return Vector3.Cross(a, b).normalized;
+    }
+    void SetFacenormal(Vector3[] v, int xsize, int ysize, Vector3[] trinormals)
+    {
+        for (int tn = 0, vi = 0, y = 0; y < ysize; y++, vi++)
+        {
+            for (int x = 0; x < xsize; x++, tn += 2, vi++)
+            {
+                trinormals[tn] = FaceNormal(v, vi, xsize + 1 + vi, 1 + vi);
+                trinormals[tn + 1] = FaceNormal(v, vi + 1, xsize + 1 + vi, xsize + 2 + vi);
+            }
+        }
+    }
+    List<int> FindTriangles(int[] t, int tempv)
+    {
+        List<int> tempresult=new List<int>();
+        tempresult.Clear();
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (t[i] == tempv) { tempresult.Add(i / 3); }
+        }
+        return tempresult;
+    }
+    Vector3 TrianglesToNormals(List<int> triangles,Vector3[] triNormals)
+    {
+        Vector3 tempn=new Vector3();
+        for(int i = 0; i < triangles.Count; i++)
+        {
+            tempn+= triNormals[triangles[i]];
+        }
+        tempn=tempn.normalized;
+        return tempn;
+    }
+
+    void ComputeNormals(Vector3[] v,int[] t, Vector3[] n)
+    {
+        Vector3[] triNormals = new Vector3[xSize * ySize*2];//each triangle's noraml
+        SetFacenormal(v, xSize, ySize, triNormals);
+
+        //now we need to use triNormals to update each vertex's normal
+        for (int i = 0; i < v.Length; i++)
+        {
+            List<int> templist = FindTriangles(t,i);
+            n[i]=TrianglesToNormals( templist, triNormals);
+        }
+        //UpdateNormals(v, n);
+    }
+    LineSegment[] mNormals;
+    //initialize the cylinder on the sphere of each vertex
+    void InitNormals(Vector3[] v, Vector3[] n)
+    {
+        mNormals = new LineSegment[v.Length];
+        for (int i = 0; i < v.Length; i++)
+        {
+            GameObject o = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            mNormals[i] = o.AddComponent<LineSegment>();
+            mNormals[i].SetWidth(0.05f);
+            mNormals[i].transform.SetParent(this.transform);
+        }
+        UpdateNormals(v, n);
+    }
+    void UpdateNormals(Vector3[] v, Vector3[] n)
+    {
+        for (int i = 0; i < v.Length; i++)
+        {
+            mNormals[i].SetEndPoints(v[i], v[i] + 1.0f * n[i]);
+        }
+    }
+}
